44fc8c56e519756be196ecd4c7b2e0bdab229f7a
akshay _m_ <168826063+Mobing-ms@users.noreply.github.com>
2026-05-25 15:49:10 +0530

    Add files via upload
    solution for longest prefix problem. (Runtime(0ms))

---
+++ common_prefix.py
@@ -0,0 +1,21 @@
+from collections import deque
+
+class Solution(object):
+    def longestCommonPrefix(self, strs):
+        
+        k = 0
+        cd = []
+        list1 = strs.copy()
+
+        for i in list1:
+            j = i+1
+            for j in lists:
+                if i[k] = j[k]
+                    cd.append(i[k])
+                
+        print(cd)
+
+
+longstr = ['apple','appi']
+sol = Solution()
+sol.longestCommonPrefix(longstr)
